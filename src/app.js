b7c3462a3de94464c22b7a658f82be149de4626c
kase702 <ichmagkase70@gmail.com>
2025-09-09 00:33:35 -0400

    initial website

---
+++ src/app.js
@@ -0,0 +1,144 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+function sayHello() {
+  console.log("starting transition");
+  let planetContainer = document.getElementById("planet-container");
+  planetContainer.style.animationName = 'animate-planet';
+
+  let transitionButtonContainer = document.getElementById("content-container");
+  transitionButtonContainer.style.animationName = 'animate-button';
+
+  let transitionButton = document.getElementById("transition-button");
+  transitionButton.style.animationName = 'button-to-transparent';
+
+  let spacer = document.getElementById("spacer");
+  spacer.style.animationName = 'remove-spacer';
+
+  let content = document.getElementById("content");
+  content.style.animationName = 'show-content';
+}
+
+particlesJS('particles-js',
+
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": ["#BAA2E1", "#CA8FD2", "#C284B1", "#F595AA", "#AAAAAA"]
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.1,
+        "direction": "left",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 5,
+          "duration": 0.1,
+          "opacity": 5,
+          "speed": 1
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 1
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+  }
+
+);
+
+document.getElementById("transition-button").addEventListener("click", sayHello)
