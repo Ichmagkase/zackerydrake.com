8f7fa31b3bb096538541241b6dad12352736224c
kase702 <ichmagkase70@gmail.com>
2026-02-06 00:42:06 -0500

    Another overhaul

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Zackery Drake";
+export const SITE_DESCRIPTION =
+  "Hello, I'm Zack! A student and aspiring Software Engineer. This is my Portfolio site!";
